5e64382ad40211b5c52c34604626b872ff42e049
Sweet Lea Atay <jesterabalos244@gmail.com>
2025-10-18 01:22:10 +0800

    Update website design/content

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Dashboard from './dashboard.jsx'
+import App from './App.jsx'  // ✅ DAPAT ITO, HINDI DASHBOARD
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>,
 )
